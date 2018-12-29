b11843e5361a5c7db2bf7c31bb41ac59295a19b8
honda <mh35jp@gmail.com>
2018-12-30 08:56:47 +0900

    Register sidebar 1

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,11 @@
 <div class="sidebar-wrap">
-    <div id="sidebar" class="sidebar-content"></div>
+    <div id="sidebar" class="sidebar-content">
+    <?php if (is_active_sidebar('sidebar-1')) {
+        ?>
+        <ul class="sidebar-list">
+        <?php dynamic_sidebar('sidebar-1'); ?>
+        </ul>
+        <?php
+    } ?>
+    </div>
 </div>
