5973860c347720baf0d166941730f87007c6bbc2
honda <mh35jp@gmail.com>
2018-12-30 09:31:50 +0900

    Templates

---
+++ archive.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+<h1 class="archive-title"><?php the_archive_title(); ?></h1>
+<?php if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <?php if (is_singular()) {
+            ?>
+            <h1 class="post-title"><?php the_title(); ?></h1>
+            <?php
+        } else {
+            ?>
+            <h2 class="post-title">
+                <a href="<?php the_permalink();
+                ?>"><?php the_title(); ?></a>
+            </h2>
+            <?php
+        } ?>
+        <div class="post-content"><?php the_content(); ?></div>
+        </article>
+        <?php
+    }
+} ?>
+<?php get_footer(); ?>
