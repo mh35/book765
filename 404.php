179e6db1359b726f396c451a90306250d49f8cfc
honda <mh35jp@gmail.com>
2018-12-30 09:34:40 +0900

    404 template

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+<article id="post-0" class="post-not-found">
+    <h1 class="post-title"><?php _e('Not found', 'book765'); ?></h1>
+    <div class="post-content"><?php
+    _e('Woops, the content you want does not exist.', 'book765'); ?></div>
+</article>
+<?php get_footer(); ?>
